1ba5f15b71fbbf7c15046fe253c01eb67179a8b7
Akashraj <akashrajr1@gmail.com>
2016-09-04 18:19:15 +0530

    Part A of Q 4
    The question required us to use int memRelease(int **). If I tried to pass table variable (my entire ragged array into it ), it would have a pass by value. This was the best solution I could come with. 
    If someone can improve upon it, please do

---
+++ Assignments/DS/Assignment1/Ans4/4_1.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int memRelease(int ** a)
+{	
+	free(*a);
+	*a=NULL;
+	if(*a==NULL)
+		return 1;
+	else
+		return -1;
+}
+
+void main()
+{
+	int rNum,cNum,i,j;
+	printf("Enter elements\n");
+	printf("Enter Row Number\n");
+	scanf("%d",&rNum);
+	int **table = calloc(rNum+1,sizeof(int *));
+	table[rNum]=NULL;
+	for(i=0;i<rNum;i++)
+	{
+		printf("Enter column size %d \n",i+1);
+		scanf("%d",&cNum);
+		table[i]=calloc(cNum+1,sizeof(int));
+		table[i][0]=cNum;
+		for(j=1;j<=table[i][0];j++)
+			scanf("%d",&table[i][j]);
+	}  
+	
+	printf("\nElements are\n");
+	for(i=0;i<rNum;i++)
+		{for(j=1;j<=table[i][0];j++)
+			printf("%d ",table[i][j]);	
+		 printf("\n");}
+		 	
+	for(i=0;i<rNum;i++)
+		j=memRelease(&table[i]);
+
+	free(table);
+	if(table=NULL)
+	printf("NULLLL");
+
+}
