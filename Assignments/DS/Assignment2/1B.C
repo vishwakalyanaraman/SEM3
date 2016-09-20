8de906a1a4a2b301f1b296afcf98a27e24a014f1
Ronald <ronald1das@gmail.com>
2016-09-20 21:53:25 +0530

    Added Assignment 2 Maths

---
+++ Assignments/DS/Assignment2/1B.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int Fib(int n){
+	if(n ==0 || n==1)
+	{
+		return n;
+	}
+	else
+	{
+		return Fib(n-1)+Fib(n-2);
+	}
+}
+
+int main()
+{
+	int n,i;
+	printf("Enter limit\n");
+	scanf("%d",&n);
+	for(i = 0;i<n;i++)
+	{
+		printf("%d\n",Fib(i));
+	}
+
+}
