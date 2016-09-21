162211db6f21bd64ada0692b94444673f60b0325
Akashraj <akashrajr1@gmail.com>
2016-09-21 19:28:24 +0530

    3 A of DS II

---
+++ Assignments/DS/Assignment2/3a.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#define MAX 3
+
+int top[2]={-1,MAX};
+int a[MAX];
+int flag=0;
+
+int push(int s,int element)
+{
+    if(s==1)
+    {   if(top[0]+1==top[1] || top[0]==MAX)
+            flag=1;
+        else
+            flag=0;
+        if(flag)
+            {
+                printf("Stack Overflow\n");
+                s=0;
+                return s;
+            }
+
+        a[++top[0]]=element;
+    }
+    if(s==2)
+    {   if(top[1]-1==top[0] || top[1]==-1)
+            flag=1;
+        else
+            flag=0;
+        if(flag)
+            {
+                printf("Stack Overflow\n");
+                s=0;
+                return s;
+            }
+
+        a[--top[1]]=element;
+
+    }
+    return s;
+}
+
+void pop(s)
+{
+    if(s==1)
+    {
+        if(top[0]==-1)
+            printf("Stack Underflow\n");
+        else
+            --top[0];
+
+    }
+    if(s==2)
+    {
+         if(top[1]==MAX)
+            printf("Stack Underflow\n");
+            else
+                --top[1];
+    }
+}
+
+void display()
+{
+    int i;
+    for(i=0;i<MAX;i++)
+        printf("%d ",a[i]);
+}
+void main()
+{
+    int s,v,i=0;
+while(i!=4)
+{
+    printf("\n1.Push(s,element)\n2.Pop (s)\n3.Display\n4.Exit\n");
+    scanf("%d",&i);
+    switch(i)
+    {
+        case 1: printf("Enter values (s,element)\n");
+                scanf("%d",&s);
+                scanf("%d",&v);
+                s=push(s,v);
+                if(s==0)
+                    printf("s is set to zero");
+                break;
+        case 2: printf("Enter values (s)\n");
+                scanf("%d",&s);
+                pop(s);
+                break;
+        case 3: display();
+                break;
+        case 4:;
+    }
+}
+
+
+
+}
